f6e436ba5d378105282e05dc8f38459ff1b4b13f
Giseokkang <river.key93@gmail.com>
2019-06-17 20:33:00 +0900

    mongoose

--- router/userRouter.js
+++ router/userRouter.js
@@ -10,8 +10,8 @@ import {
 const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, ChangePassword);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
