9df54836d79eba919697af3b06a7cf60f8353251
Giseokkang <river.key93@gmail.com>
2019-06-18 19:12:00 +0900

    Uploading and Creating a Video part Two

---
+++ db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("😜   Connected to DB");
+const handleError = error => console.log(`X Error on DB connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
