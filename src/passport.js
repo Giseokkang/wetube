caf6aa70f8220538cb690a87a6ac4ee1a462cd61
Giseokkang <river.key93@gmail.com>
2019-07-24 01:47:55 +0900

    Github login

--- src/passport.js
+++ src/passport.js
@@ -15,7 +15,9 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: process.env.RPODUCTION
+        ? `https://frozen-scrubland-99246.herokuapp.com${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
   )
